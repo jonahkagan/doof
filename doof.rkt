276598ad9975858eb0b19ec3a0d3d8f6ffab6130
jonahkagan <jonahkagan@gmail.com>
2013-02-20 16:33:54 -0500

    Added object literals, get field, extend object.

--- doof.rkt
+++ doof.rkt
@@ -6,26 +6,38 @@
  (struct-out s-app)
  (struct-out s-lam)
  (struct-out s-prim)
+ (struct-out s-obj)
+ (struct-out s-get)
+ (struct-out s-ext)
+ 
+ (struct-out v-str)
+ (struct-out v-clos)
+ (struct-out v-obj)
+ 
  parse
  (rename-out [interp-expr interp]))
 
 (require racket/match)
+;(require/typed typed/racket [hash (All (k v) (k v 
 
 ;(define-predicate sexp? Sexp))
 
-(define-type Expr (U s-str s-id s-lam s-app s-prim))
+(define-type Expr (U s-str s-id s-lam s-app s-prim s-obj s-get s-ext))
 
 (struct: s-str ([str : String]) #:transparent)
 (struct: s-id ([id : Symbol]) #:transparent)
 (struct: s-lam ([arg : Symbol] [body : Expr]) #:transparent)
 (struct: s-app ([fun : Expr] [arg : Expr]) #:transparent)
 (struct: s-prim ([name : Symbol] [arg : Expr]) #:transparent)
+(struct: s-obj ([fields : (Listof (Pairof Expr Expr))]) #:transparent)
+(struct: s-get ([obj : Expr] [field : Expr]) #:transparent)
+(struct: s-ext ([obj : Expr] [field : Expr] [val : Expr]) #:transparent)
 
-(define-type Value (U v-str v-clos))
+(define-type Value (U v-str v-clos v-obj))
 
 (struct: v-str ([str : String]) #:transparent)
 (struct: v-clos ([arg : Symbol] [body : Expr] [env : Env]) #:transparent)
-
+(struct: v-obj ([fields : (HashTable String Value)]) #:transparent)
 
 (define-type Env (Listof (Pairof Symbol Value)))
 (define mt-env empty)
@@ -38,61 +50,109 @@
     [(equal? id (car (first env))) (cdr (first env))]
     [else (lookup id (rest env))]))
 
-(define: (parse [s : Any]) : Expr
+; Really takes an Sexp, but typed/racket complained
+(define: (parse [se : Any]) : Expr
   (cond
-    [(string? s) (s-str s)]
-    [(symbol? s) (s-id s)]
-    [(list? s)
-     (match s
+    [(string? se) (s-str se)]
+    [(symbol? se) (s-id se)]
+    [(list? se)
+     (match se
        [(list 'lambda (list arg) body)
-        (if (symbol? arg) 
-            (s-lam arg (parse body))
-            (error "lambda arg not a symbol"))]
+        (cond
+          [(symbol? arg) (s-lam arg (parse body))]
+          [else (error "lambda arg not a symbol")])]
+       [(list 'obj fields ...)
+        (s-obj (map (lambda (fe)
+                      (match fe
+                        [(list name ': value)
+                         (cons (parse name) (parse value))]
+                        [_ (error "bad parse field")]))
+                    fields))]
+       [(list 'get obj field)
+        (s-get (parse obj) (parse field))]
+       [(list 'ext obj (list field ': val))
+        (s-ext (parse obj) (parse field) (parse val))]
        [(list fun arg)
-        (let ([fune (parse fun)])
-          (if (and (s-id? fune) (is-prim? (s-id-id fune)))
-              (s-prim (s-id-id fune) (parse arg))
-              (s-app (parse fun) (parse arg))))]
+        (match (parse fun)
+          [(s-id fid)
+           (cond
+             [(is-prim? fid) (s-prim fid (parse arg))]
+             [else (s-app (s-id fid) (parse arg))])]
+          [exp (s-app exp (parse arg))])]
        [_ (error "bad parse list")])]
     [else (error "bad parse")]))
 
-(define: (interp-expr [e : Expr]) : String
-  (match (interp e mt-env)
-    [(v-str s) s]
-    [else (error "interp-expr produced non-str")]))
+
+(define: (interp-expr [e : Expr]) : Value
+  (interp e mt-env))
 
 (define: (interp [e : Expr] [env : Env]) : Value
   (match e
     [(s-str s) (v-str s)]
+    
     [(s-id id)
      (let ([v (lookup id env)])
        (if v v
            (error (string-append "Unbound id: " (symbol->string id)))))]
+    
     [(s-prim name arg)
      (prim name (interp arg env) env)]
+    
     [(s-lam arg body)
      (v-clos arg body env)]
+    
     [(s-app fun arg)
-     (let ([funv (interp fun env)]
-           [argv (interp arg env)])
-       (match funv
-         [(v-clos carg cbody cenv)
-          (interp cbody
-                  (extend-env (bind carg argv) cenv))]
-         [else (error "Can't apply non-function value")]))]))
+     (match (interp fun env)
+       [(v-clos carg cbody cenv)
+        (interp cbody
+                (extend-env (bind carg (interp arg env)) cenv))]
+       [else (error "can't apply non-function value")])]
+    
+    [(s-obj fields)
+     (v-obj (make-immutable-hash
+             (map (lambda: ([f : (Pairof Expr Expr)])
+                    (match (interp (car f) env)
+                      [(v-str name) 
+                       (cons name (interp (cdr f) env))]
+                      [_ (error "field name must be a string")]))
+                  fields)))]
+    
+    [(s-get obj field)
+     (match (interp obj env)
+       [(v-obj fields)
+        (match (interp field env)
+          [(v-str name)
+           (hash-ref fields name)]
+          [_ (error "get: field name must be a string")])]
+       [_ (error "get: can't get from a non-object")])]
+    
+    [(s-ext obj field val)
+     (match (interp obj env)
+       [(v-obj fields)
+        (match (interp field env)
+          [(v-str name)
+           (v-obj (hash-set fields name (interp val env)))]
+          [_ (error "ext: field name must be a string")])]
+       [_ (error "ext: can't extend a non-object")])]
+    ))
 
+; Primitives exposed to the language user
 (define (is-prim? id)
-  (member id '(cat cat2)))
+  (member id '(cat)))
 
 (define: (prim [name : Symbol] [arg : Value] [env : Env]) : Value
   (case name
     [(cat) (v-clos 's2
-                   (s-prim '__cat2 (s-id 's2))
+                   (s-prim 'cat2 (s-id 's2))
                    (extend-env (bind 's1 arg) mt-env))]
-    [(__cat2) (let ([s (lookup 's1 env)])
-                (if (and s (v-str? s) (v-str? arg))
-                    (v-str (string-append (v-str-str s) (v-str-str arg)))
-                    (error "prim error")))]
+    [(cat2) 
+     (match (lookup 's1 env)
+       [(v-str s1)
+        (match arg
+          [(v-str s2) 
+                  (v-str (string-append s1 s2))]
+          [_ (error "cat: expected string as second arg")])]
+       [_ (error "cat: expected string as first arg")])]
     [else (error "unknown prim")]))
 
 
--- test.rkt
+++ test.rkt
@@ -14,44 +14,88 @@
 (define (check-interp-exn sexpr)
   (check-exn exn:fail? (lambda () (interp (parse sexpr)))))
 
-(define parse-tests
-  (test-suite
-   "parse"
-   
-   (check-parse '"doofus" (s-str "doofus"))
-   (check-parse 'doofid (s-id 'doofid))
-   
-   (check-parse '(apply this) (s-app (s-id 'apply) (s-id 'this)))
-   (check-parse '((cat "that") "there") (s-app
-                                         (s-prim 'cat (s-str "that"))
-                                         (s-str "there")))
-   
-   (check-parse '(lambda (x) x) (s-lam 'x (s-id 'x)))
-   (check-parse '(lambda (y) "why") (s-lam 'y (s-str "why")))
-   
-   (check-parse '((lambda (x) x) "ecks")
-                (s-app (s-lam 'x (s-id 'x)) (s-str "ecks")))
-   ))
+;; Parse tests
 
-(define interp-tests
-  (test-suite
-   "interp"
-   
-   (check-interp '"doofus" "doofus")
-   (check-interp-exn 'doofid) ; unbound id
-   
-   (check-interp '((cat "lol") "ol") "lolol")
-   (check-interp '((lambda (x) x) "ecks") "ecks")
-   (check-interp '(((lambda (x)
-                    (lambda (y)
-                      ((cat x) y)))
-                  "hey") "ho")
-               "heyho")
-   (check-interp '(((lambda (x)
-                    (lambda (x) x))
-                  "shade") "shadow")
-               "shadow")
-   ))
+(check-parse '"doofus" (s-str "doofus"))
+(check-parse 'doofid (s-id 'doofid))
 
-(run-tests parse-tests)
-(run-tests interp-tests)
+(check-parse '(apply this) 
+             (s-app (s-id 'apply) (s-id 'this)))
+(check-parse '((cat "that") "there")
+             (s-app
+              (s-prim 'cat (s-str "that"))
+              (s-str "there")))
+
+(check-parse '(lambda (x) x)
+             (s-lam 'x (s-id 'x)))
+(check-parse '(lambda (y) "why")
+             (s-lam 'y (s-str "why")))
+
+(check-parse '((lambda (x) x) "ecks")
+             (s-app (s-lam 'x (s-id 'x)) (s-str "ecks")))
+
+(check-parse '(obj)
+             (s-obj '()))
+(check-parse '(obj ("first" : Doof) (last : "Us"))
+             (s-obj (list (cons (s-str "first") (s-id 'Doof))
+                          (cons (s-id 'last) (s-str "Us")))))
+(check-parse '(obj (((cat "do") "of") : (f "us")))
+             (s-obj (list (cons (s-app (s-prim 'cat (s-str "do"))
+                                       (s-str "of"))
+                                (s-app (s-id 'f) (s-str "us"))))))
+
+(check-parse '(get (obj) "f")
+             (s-get (s-obj '()) (s-str "f")))
+(check-parse '(get o f)
+             (s-get (s-id 'o) (s-id 'f)))
+
+(check-parse '(ext (obj) ("f" : "v"))
+             (s-ext (s-obj '()) (s-str "f") (s-str "v")))
+(check-parse '(ext o (f : v))
+             (s-ext (s-id 'o) (s-id 'f) (s-id 'v)))
+
+
+;; Interp tests
+
+(define doofus (v-str "doofus"))
+(define heyho (v-str "heyho"))
+
+(check-interp '"doofus" doofus)
+(check-interp-exn 'doofid) ; unbound id
+
+(check-interp '((cat "doof") "us") doofus)
+(check-interp '((lambda (x) x) "doofus") doofus)
+(check-interp '(((lambda (x)
+                   (lambda (y)
+                     ((cat x) y)))
+                 "hey") "ho")
+              heyho)
+(check-interp '(((lambda (x)
+                   (lambda (x) x))
+                 "let's go") "heyho")
+              heyho)
+
+(define o1 (v-obj (make-immutable-hash (list (cons "doofus" heyho)))))
+(define o2 (v-obj (make-immutable-hash 
+                   (list (cons "doofus" heyho) (cons "heyho" doofus)))))
+
+(check-interp '(obj) (v-obj (hash)))
+(check-interp '(obj ("doofus" : "heyho") ("heyho" : "doofus"))
+              o2)
+(check-interp '(obj (((cat "doof") "us") : ((lambda (x) x) "heyho")))
+              o1)
+
+(check-interp '(get (obj ("doofus" : "heyho")) "doofus" )
+              heyho)
+(check-interp-exn '(get (obj)  "doofus")) ; field not found
+(check-interp '(get (obj ("heyho" : "doofus")
+                         ("doofus" : "heyho"))
+                    ((cat "doof") "us"))
+              heyho)
+
+(check-interp '(ext (obj) ("doofus" : "heyho"))
+              o1)
+(check-interp '(ext (obj ("doofus" : "heyho")) ("heyho" : "doofus"))
+              o2)
+(check-interp '(ext (obj ("doofus" : "heyho")) ("doofus" : "doofus"))
+              (v-obj (make-immutable-hash (list (cons "doofus" doofus)))))
